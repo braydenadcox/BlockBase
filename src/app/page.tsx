020cac9c30dd75ba91043cc2296581f10b741d95
Brayden Adcox <braydenadcox@gmail.com>
2025-05-31 16:58:49 -0700

    Popular sets section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 "use client"
 import Navbar from '@/components/navbar';
 import HomeCarousel from '@/components/homeCarousel';
+import { UpcomingCarousel, PopularCarousel } from '@/components/homeAuxCarousels';
 
 export default function Home() {
   return (
-    <main className='h-[1500px] bg-white'>
+    <main className='h-[2300px] bg-white'>
       <Navbar />
       <HomeCarousel />
+      <PopularCarousel />
     </main>
   )
 };
---
+++ src/components/homeAuxCarousels.tsx
@@ -0,0 +1,23 @@
+"use client"
+import { useState } from "react";
+
+export const UpcomingCarousel = () => {
+    return (
+        <div>
+            <p>filler</p>
+        </div>
+    );
+};
+
+export const PopularCarousel = () => {
+    return (
+        <div className='flex w-full h-[450px] justify-evenly py-8 px-4'>
+            <div className="flex-1 mx-4 bg-red-500"></div>
+            <div className="flex-1 mx-4 bg-orange-500"></div>
+            <div className="flex-1 mx-4 bg-yellow-500"></div>
+            <div className="flex-1 mx-4 bg-green-500"></div>
+            <div className="flex-1 mx-4 bg-blue-500"></div>
+            <div className="flex-1 mx-4 bg-purple-500"></div>
+        </div>
+    );
+};
