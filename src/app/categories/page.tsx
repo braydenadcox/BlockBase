8d53aac85a0e47197cf59ee99d2e296ea62a3c24
Brayden Adcox <braydenadcox@gmail.com>
2025-06-22 23:43:56 -0700

    Touch-up

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -13,12 +13,12 @@ export default function Categories() {
         </div>
         <div className='flex w-full h-[490px] align-center justify-center'>
           <Link className='flex-1 bg-red-500' href='/shop?group=Star Wars'>STAR WARS</Link>
-          <Link className='flex-1 bg-orange-500' href='/shop'>ARCHITECTURE</Link>
-          <Link className='flex-1 bg-yellow-500' href='/shop'>HARRY POTTER</Link>
-          <Link className='flex-1 bg-green-500' href='/shop'>CITY</Link>
-          <Link className='flex-1 bg-blue-500' href='/shop'>18+</Link>
+          <Link className='flex-1 bg-orange-500' href='/shop?group=Architecture'>ARCHITECTURE</Link>
+          <Link className='flex-1 bg-yellow-500' href='/shop?group=Harry Potter'>HARRY POTTER</Link>
+          <Link className='flex-1 bg-green-500' href='/shop?group=City'>CITY</Link>
+          <Link className='flex-1 bg-blue-500' href='/shop?group=18+'>18+</Link>
           <Link className='flex-1 bg-purple-500' href='/shop?group=Minecraft'>MINECRAFT</Link>
-          <Link className='flex-1 bg-pink-500' href='/shop'>NINJAGO</Link>
+          <Link className='flex-1 bg-pink-500' href='/shop?group=Ninjago'>NINJAGO</Link>
         </div>
       </main>
       <Footer />
